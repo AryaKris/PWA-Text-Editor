d17db2ac20c20693b8ad23d93dd2984986c57cfb
Arya Krishna <dr.aryakris@outlook.com>
2022-03-22 19:21:57 -0700

    debugging

--- develop/client/src/js/database.js
+++ develop/client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const textDb = await openDB('jate', 1);
   const tx = textDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, value: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
@@ -31,7 +31,7 @@ export const getDb = async () => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.getAll();
+  const request = store.get(1);
 
   const result = await request;
   console.log('result.value', result);
